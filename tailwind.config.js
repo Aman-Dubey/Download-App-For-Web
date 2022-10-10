976bbf62a574bf55eb755471139e5283625276d9
Aman Dubey <aman@Amans-MacBook-Pro.local>
2022-10-10 23:49:52 +0530

    Basic Home page for an app download page

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors : {
+        strongCyan: 'hsl(171, 66%, 44%)',
+        lightBlue: 'hsl(233, 100%, 69%)',
+        darkGrayishBlue: 'hsla(210, 10%, 33%, 1)',
+        grayishBlue: 'hsl(201, 11%, 66%)',
+      },
+      screens : {
+        nl: '570px',
+      }
+    },
+    fontFamily : {
+      sans : ['Bai Jamjuree, sans-serif']
+    },
+  },
+  plugins: [],
+}
+
